84d536a0c37902afa9b465e316403e734481c65e
Liam Sauve <liam.sauve@gmail.com>
2025-07-29 14:16:28 -0400

    init

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class GOLEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    GOLEQAudioProcessorEditor (GOLEQAudioProcessor&);
+    ~GOLEQAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    GOLEQAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GOLEQAudioProcessorEditor)
+};
